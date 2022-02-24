d4417a73f896ddc4fe588eefa0bcefbb264b53ce
bun <janin@foxmail.com>
2022-02-24 10:12:15 +0800

    feat: 启动广播可能接收不到

--- app/src/main/java/top/xmbun/auto/car/StartupBroadcastReceiver.java
+++ app/src/main/java/top/xmbun/auto/car/StartupBroadcastReceiver.java
@@ -5,16 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
+import android.util.Log;
 
 public class StartupBroadcastReceiver extends BroadcastReceiver {
 
+    private static final String TAG = "StartupReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "接收到了系统启动消息", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "接收到了系统启动消息");
         final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
         final boolean autoload = sp.getBoolean("autoload_on_boot", false);
-        if (autoload) {
+        if (autoload || true) {
             WifiUtils.tureOnWifi(context);
         }
     }
