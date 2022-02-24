3c92905756e066ea34bfac94c493a19f5520c43b
bun <janin@foxmail.com>
2022-02-24 16:24:45 +0800

    doc: 补充注释

--- app/src/main/java/top/xmbun/auto/car/StartupBroadcastReceiver.java
+++ app/src/main/java/top/xmbun/auto/car/StartupBroadcastReceiver.java
@@ -7,6 +7,12 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+/**
+ * 开机广播接收器.
+ *
+ * @author bun
+ * @since 2022-02-18
+ */
 public class StartupBroadcastReceiver extends BroadcastReceiver {
 
     private static final String TAG = "StartupReceiver";
@@ -15,8 +21,8 @@ public class StartupBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "接收到了系统启动消息");
         final SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-        final boolean autoload = sp.getBoolean("autoload_on_boot", false);
-        if (autoload || true) {
+        final boolean autoload = sp.getBoolean(SettingsActivity.WLAN_AUTOLOAD_ON_BOOT, false);
+        if (autoload) {
             WifiUtils.tureOnWifi(context);
         }
     }
--- app/src/main/java/top/xmbun/auto/car/WifiUtils.java
+++ app/src/main/java/top/xmbun/auto/car/WifiUtils.java
@@ -4,10 +4,21 @@ import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 
+/**
+ * 网络工具.
+ *
+ * @author bun
+ * @since 2022-02-18
+ */
 public class WifiUtils {
 
     private static final String TAG = "WifiUtils";
 
+    /**
+     * 打开网络.
+     *
+     * @param context 上下文
+     */
     public static void tureOnWifi(Context context) {
         final WifiManager wifiManager = (WifiManager) context.getApplicationContext()
                 .getSystemService(Context.WIFI_SERVICE);
@@ -18,6 +29,11 @@ public class WifiUtils {
         Log.d(TAG, "当前wifi: " + wifiManager.isWifiEnabled());
     }
 
+    /**
+     * 关闭网络.
+     *
+     * @param context 上下文
+     */
     public static void tureOffWifi(Context context) {
         final WifiManager wifiManager = (WifiManager) context.getApplicationContext()
                 .getSystemService(Context.WIFI_SERVICE);
