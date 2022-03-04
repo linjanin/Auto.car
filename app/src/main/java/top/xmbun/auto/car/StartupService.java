09a77ddb0fd30af9e9c3177be72c69a5a5194764
bun <janin@foxmail.com>
2022-03-04 11:08:33 +0800

    feat: 开机处理服务的异常处理

--- app/src/main/java/top/xmbun/auto/car/StartupService.java
+++ app/src/main/java/top/xmbun/auto/car/StartupService.java
@@ -1,11 +1,16 @@
 package top.xmbun.auto.car;
 
 import android.app.IntentService;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * 开机启动服务.
@@ -23,16 +28,36 @@ public class StartupService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        // 准备 UI 线程环境
+        final Context context = this;
+        final Handler handler = new Handler(Looper.getMainLooper());
+
         // 网络
-        wlan();
+        try {
+            wlan();
+        } catch (Throwable e) {
+            Log.e(TAG, "操作网络失败", e);
+        }
 
         // 蓝牙
-        bluetooth();
+        try {
+            bluetooth();
+        } catch (Throwable e) {
+            Log.e(TAG, "操作蓝牙失败", e);
+        }
 
         // 导航
-        navigation();
+        try {
+            navigation();
+        } catch (Throwable e) {
+            Log.e(TAG, "操作导航失败", e);
+            handler.post(() -> {
+                Toast.makeText(context,
+                        "开启导航失败: " + e.getLocalizedMessage(), Toast.LENGTH_LONG)
+                        .show();
+            });
+        }
     }
-
 
     /**
      * 网络自启.
@@ -69,7 +94,10 @@ public class StartupService extends IntentService {
                 SettingsActivity.NAVIGATION_AUTOLOAD_ON_BOOT, autoload,
                 SettingsActivity.NAVIGATION_APP_INFO, packageName));
         if (autoload && !TextUtils.isEmpty(packageName)) {
-            final Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+            Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+            if (intent == null) {
+                throw new ActivityNotFoundException("package = " + packageName);
+            }
             startActivity(intent);
         }
     }
